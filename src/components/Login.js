161f821cb36b644cac9a94fa1b1b523e7151458a
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-29 11:46:40 -0500

    adds to instruction text in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,6 +20,6 @@ export default Login;
 //Task List:
 //1. Build a form containing a username and password field.
 //2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEST "username" and "password" RESPECTIVELY.
+//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
 //4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
