554e78404fce6622e665eaf57560c586971142fa
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-21 13:13:52 -0400

    updates login.js.

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,6 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  useEffect(()=>{
-    // make a post request to retrieve a token from the api
-    // when you have handled the token, navigate to the BubblePage route
-  });
-  
   const error = "";
   //replace with error state
 
@@ -31,4 +26,4 @@ export default Login;
 //2. Add whatever state nessiary for form functioning.
 //3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
 //4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
-//5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+//5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
